ac8a2b6c41e45d35dbbbbdcf9df1a2a5b3d77224
Mikhail Pinteley <mihailpintelei@yandex.ru>
2025-12-01 04:32:15 +0300

    Второй этап 1.0.0

--- ewm-service/src/main/java/ru/practicum/ewmservice/exception/ConditionsNotMetException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.ewmservice.exception;
-
-public class ConditionsNotMetException extends RuntimeException {
-    public ConditionsNotMetException(String message) {
-        super(message);
-    }
-}
